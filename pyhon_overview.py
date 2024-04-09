b2484264263ed9cf52d119bec0c29a40d02a07ae
sif7 <sif7@outlook.com>
2024-04-09 23:16:12 +0100

    ongoing

--- pyhon_overview.py
+++ pyhon_overview.py
@@ -1,8 +1,14 @@
-# string literal
+import io
+from random import shuffle
+from random import randint
+from pprint import pprint  # pretty print
+
+print("\n---string literal")
 my_money = 50
-my_spend = 15*1.03
+my_spend = 15 * 1.03
 print(f"I have ${my_money-my_spend} left")
 
+"""
 # try / except
 while True:
     try:
@@ -15,34 +21,61 @@ while True:
         break
     finally:
         print("I always get printed")
+"""
 
-ls1 = [100,101,102,103,104,105]
-# slicing list
-print (ls1[:2])
-print (ls1[:3])
-print (ls1[::])   # print all of list
-print (ls1[::-1]) # reverse list
-print (ls1[-1])   # last item
-print (ls1[0])
-print (ls1[-6])   # 0 and -6 equivalent in list with 6 items
+print("\n---slices")
+ls_test_1 = [100, 101, 102, 103, 104, 105]
+print(ls_test_1[:2])
+print(ls_test_1[:3])
+print(ls_test_1[::])  # print all of list
+print(ls_test_1[::-1])  # reverse list
+print(ls_test_1[-1])  # last item
+print(ls_test_1[0])
+print(ls_test_1[-6])  # 0 and -6 equivalent in list with 6 items
 
-# for loops (for, enumerate and range)
-for item in ls1:
+print("\n---for loop")
+for item in ls_test_1:
     print(f"{item}")
-for idx,item in enumerate(ls1):
+print("\n---for loop with enumerate")
+for idx, item in enumerate(ls_test_1):
     print(f"{idx}: {item}")
-for i in range(0,50):
-    print(f"{i}",end=" ")
-print()
+ls_long_1 = []
+print("\n---for loop with range")
+for i in range(0, 50):
+    # print(f"{i}", end=" ")
+    ls_long_1.append(i)
+print(
+    *ls_long_1, sep=" ", end="\n"
+)  # astrix unpacks the list into a new list without formatting
+print("\n---unpacking a list into specific variables then the reamining into *rest")
+first, second, *rest = ls_long_1
+print(f"first={first}, second={second}, rest={rest}")
 
-# pop(), remove by index
-ls1.pop() # pops -1 by default
-for idx,item in enumerate(ls1):
+print("\n---tuple unpacking")
+list_of_tups_1 = [(1, 2), (3, 4), (5, 6), (7, 8)]
+for idx, (x, y) in enumerate(list_of_tups_1):
+    print(f"{idx}: {x} {y}")
+
+print("\n---tuple packing via zip (opposite of enumerate)")
+tuple_1 = (1, 2, 3, 4, 5, 6)
+tuple_2 = (11, 12, 13, 14, 15, 16, 17, 18)
+print(list(zip(tuple_1, tuple_2)))
+
+print("\n---IN PLACE, pop(), remove by index")
+ls_test_1.pop()  # pops -1 by default
+for idx, item in enumerate(ls_test_1):
     print(f"{idx}: {item}")
-ls1.pop(0)
-for idx,item in enumerate(ls1):
+ls_test_1.pop(0)
+for idx, item in enumerate(ls_test_1):
     print(f"{idx}: {item}")
-# remove(), remove by value
-ls1.remove(101) # remove a value from the list
-for idx,item in enumerate(ls1):
+print("\n---IN PLACE, remove(), remove by value")
+ls_test_1.remove(101)  # remove a value from the list
+for idx, item in enumerate(ls_test_1):
     print(f"{idx}: {item}")
+
+
+print("\n---IN PLACE shuffle")
+shuffle(ls_long_1)
+print((ls_long_1))
+rand_int = randint(1,100) 
+print(f"rand_int={rand_int}")
